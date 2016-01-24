b6a268c8ff6c7bc710fbf9fd497f4904fda5722b
amanda <amandnha21@hotmail.com>
2016-01-24 20:06:19 -0200

    Criado painel adm

---
+++ js/paineladm.js
@@ -0,0 +1,52 @@
+$(function() {
+$('.required-inserir').tooltip({
+placement: 'rigth',
+title: 'Inserir'
+});
+})
+
+$(function() {
+$('.required-editar').tooltip({
+placement: 'rigth',
+title: 'Editar'
+});
+})
+
+$(function() {
+$('.required-excluir').tooltip({
+placement: 'rigth',
+title: 'Excluir'
+});
+})
+
+function showMessageToast(message, temp) {
+$().toastmessage('showToast', {
+text: "" + message + "",
+stayTime: temp, position: 'middle-center',
+type: 'notice'
+});
+}
+
+function MessageDelete()
+{
+var msg = 'Excluido com sucesso!';
+showMessageToast(msg, 15000);
+}
+
+function MessageCadastro()
+{
+var msg = 'Cadastro realizado com sucesso!';
+showMessageToast(msg, 15000);
+}
+
+function alterar(){$("#modalPlanta").modal('show');}
+
+function alterarUsuario(){$("#modalUsuario").modal('show');}
+
+
+function alterarPerfil(){$("#modalPerfil").modal('show');}
+
+function alterarPerfilUser(){$("#modalPerfilUser").modal('show');}
+
+
+
